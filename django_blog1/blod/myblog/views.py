b0d2c1ce9552c6491f3ece48a7e79f55cbe63faa
BliahaMuha <BliahaMuha@users.noreply.github.com>
2022-12-04 22:50:08 +0300

    Добавлены посты, переделана пагинация и home.html

--- django_blog1/blod/myblog/views.py
+++ django_blog1/blod/myblog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import Post
+from django.core.paginator import Paginator
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
         return render(request,
         'home.html'
         )
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+        paginator = Paginator(posts, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'home.html', context={
+            'page_obj': page_obj
+        })
 # Create your views here.
+""" class MainView(View):
+    def get(self,request,*args, **kwargs):
+        posts = Post.objects.all() """
